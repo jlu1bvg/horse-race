db44a9db04cfada3a8c6f2feb8e7acb3cce96e6c
jeffluuu <jefflu235@outlook.com>
2024-01-22 10:22:57 -0500

    toLowerCase

--- Horse Racing Assignment/src/horseracing/Player.java
+++ Horse Racing Assignment/src/horseracing/Player.java
@@ -29,7 +29,7 @@ public class Player {
 
     public void placeBet(String betType, int amount, BettingOdds odds, List<Horse> horses){
         if(money >= amount){
-            if (betType.equals("win")){
+            if (betType.toLowerCase().equals("win")){
                 System.out.println("Which horse do u choose? (enter name or number)");
                 String horseName=console.nextLine();
                 Horse horse=null;
@@ -47,7 +47,7 @@ public class Player {
                 potentialEarnings.put(horse.getName(), (int)(amount*odds.getOdds(horse)));
                 money -= amount;
                 System.out.println("Bet of " + amount + " placed on " + horse.getName() + " to come first place");
-            }else if (betType.equals("place")){
+            }else if (betType.toLowerCase().equals("place")){
                 System.out.println("Which horse do u choose? (say name)");
                 String horseName = console.nextLine();
                 Horse horse=null;
@@ -65,7 +65,7 @@ public class Player {
                 potentialEarnings.put(horse.getName(), (int)(amount*odds.getOdds(horse)));
                 money -= amount;
                 System.out.println("Bet of " + amount + " placed on " + horse.getName() + " to come first or second place");
-            }else if (betType.equals("show")){
+            }else if (betType.toLowerCase().equals("show")){
                 System.out.println("Which horse do u choose? (say name)");
                 String horseName = console.nextLine();
                 Horse horse=null;
@@ -83,9 +83,9 @@ public class Player {
                 potentialEarnings.put(horse.getName(), (int)(amount*odds.getOdds(horse)));
                 money -= amount;
                 System.out.println("Bet of " + amount + " placed on " + horse.getName() + " to come first, second or third place");
-            }else if (betType.equals("box")){
+            }else if (betType.toLowerCase().equals("box")){
                 
-            }else if (betType.equals("exacta")){
+            }else if (betType.toLowerCase().equals("exacta")){
                 
             }
         }
